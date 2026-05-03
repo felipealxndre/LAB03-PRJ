a652e136d82bb0ca2c5a403802585cc8a7a36995
Felipe Miranda <felipe.alexandre@nubank.com.br>
2026-05-03 17:15:19 -0300

    removing close all parameter

--- main.m
+++ main.m
@@ -113,7 +113,6 @@ for k = 1 : size(casos, 1)
 
     params = struct('Vc_sub_fpm', Vc_sub_fpm, 'distancia_NM', distancia_3);
     Exportar_Resultados(caso, V_vento, heli, missao, total_comb_gasto, polar, cruzeiro, output_folder, params);
-    close all;
 end
 
 fprintf('\nTodos os casos concluídos.\n');
